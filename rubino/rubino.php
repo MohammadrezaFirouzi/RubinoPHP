84aa33546f1cc486a4f93da8b34bc98a40eb5c5b
محمدرضا فیروزی <109149767+MohammadrezaFirouzi@users.noreply.github.com>
2024-07-28 14:54:27 +0330

    Update rubino.php

--- rubino/rubino.php
+++ rubino/rubino.php
@@ -32,7 +32,7 @@ class RubinoPHP
             "method" => $method
         ];
 
-        return request($data);
+        return sendRequest($data);
     }
 
     public function getPostByShareLink(string $post_url)
@@ -214,16 +214,190 @@ class RubinoPHP
         ], "getMyProfileInfo");
     }
 
-    public function getShareLink($post_id ,$post_profile_id ,$profile_id = null)
+    public function getShareLink($post_id, $post_profile_id, $profile_id = null)
     {
         return self::run([
             "post_id" => $post_id,
-            "post_profile_id"=> $post_profile_id,
+            "post_profile_id" => $post_profile_id,
             "profile_id" => $profile_id
         ], "getShareLink");
     }
+
+    public function request_upload_file($file_name, $file_type, $profile_id)
+    {
+        return self::run([
+            "file_name" => $file_name,
+            "file_size" => filesize($file_name),
+            "file_type" => $file_type,
+            "profile_id" => $profile_id
+        ], "requestUploadFile");
+    }
+
+    public function uploadFile($file, $profile_id, $file_type, $file_name = null, $chunk = 1048576, $callback = null)
+    {
+        $pr = self::request_upload_file($file, $file_type, $profile_id)['data'];
+
+        $chunk_size = 131072;
+        $file_content = file_get_contents($file);
+        $total_parts = (int) ceil(strlen($file_content) / $chunk_size);
+
+        for ($part_number = 1; $part_number <= $total_parts; $part_number++) {
+            $start = ($part_number - 1) * $chunk_size;
+            $end = min($part_number * $chunk_size, strlen($file_content));
+            $data = substr($file_content, $start, $end - $start);
+            $headers = array(
+                'Host: ' . str_replace("/UploadFile.ashx", "", str_replace("https://", "", $pr['server_url'])),
+                'Connection: keep-alive',
+                'Content-Length: ' . strval(strlen($data)),
+                'Content-Type: application/octet-stream',
+                'auth: ' . $this->auth,
+                'file-id: ' . $pr["file_id"],
+                'total-part: ' . strval($total_parts),
+                'part-number: ' . strval($part_number),
+                'hash-file-request: ' . $pr["hash_file_request"],
+                'user-agent: okhttp/3.12.12',
+
+            );
+
+            $options = array(
+                CURLOPT_URL => $pr['server_url'],
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_POSTFIELDS => $data,
+                CURLOPT_HTTPHEADER => $headers
+            );
+
+            $curl = curl_init();
+            curl_setopt_array($curl, $options);
+            $response = json_decode(curl_exec($curl), true);
+
+            curl_close($curl);
+            if ($response['data'] != null) {
+                return array($response['data']['hash_file_receive'], $pr['file_id']);
+            }
+        }
+    }
+
+    private function add_post($file, $caption, $file_type, $profile_id, $tumb_image = null, $duration = null)
+    {
+        $payload = [
+            'rnd' => rand(10000, 99999),
+            'width' => 720,
+            'height' => 720,
+            'caption' => $caption,
+            'post_type' => $file_type,
+            'profile_id' => $profile_id,
+            'is_multi_file' => false,
+        ];
+        $result = self::uploadFile($file, $profile_id, $file_type);
+
+        $payload['file_id'] = $result[1];
+        $payload['hash_file_receive'] = $result[0];
+        $payload['thumbnail_file_id'] = $result[1];
+        $payload['thumbnail_hash_file_receive'] = $result[0];
+        if ($file_type == "Picture" && $tumb_image != null) {
+            $result_upload_thumb = self::uploadFile($tumb_image, $profile_id, 'Picture');
+            $payload['thumbnail_file_id'] = $result_upload_thumb[1];
+            $payload['thumbnail_hash_file_receive'] = $result_upload_thumb[0];
+        }
+
+        if ($file_type == "Video") {
+
+            $result_upload_thumb = self::uploadFile($tumb_image, $profile_id, 'Picture');
+            $payload['thumbnail_file_id'] = $result_upload_thumb[1];
+            $payload['thumbnail_hash_file_receive'] = $result_upload_thumb[0];
+            $payload['snapshot_file_id'] = $result_upload_thumb[1];
+            $payload['snapshot_hash_file_receive'] = $result_upload_thumb[0];
+            $payload['duration'] = $duration;
+        }
+
+        return self::run($payload, "addPost");
+    }
+
+
+    private function add_story($file, $file_type, $profile_id, $tumb_image = null, $duration = null)
+    {   
+        /** */
+       
+        $payload = [
+            'rnd' => rand(10000, 99999),
+            'width' => 720,
+            'height' => 1280,
+            'story_type' => $file_type,
+            'profile_id' => $profile_id,
+        ];
+        $result = self::uploadFile($file, $profile_id, $file_type);
+
+        $payload['file_id'] = $result[1];
+        $payload['hash_file_receive'] = $result[0];
+        $payload['thumbnail_file_id'] = $result[1];
+        $payload['thumbnail_hash_file_receive'] = $result[0];
+        if ($file_type == "Picture" && $tumb_image != null) {
+            $result_upload_thumb = self::uploadFile($tumb_image, $profile_id, 'Picture');
+            $payload['thumbnail_file_id'] = $result_upload_thumb[1];
+            $payload['thumbnail_hash_file_receive'] = $result_upload_thumb[0];
+        }
+
+        if ($file_type == "Video") {
+
+            $result_upload_thumb = self::uploadFile($tumb_image, $profile_id, 'Picture');
+            $payload['thumbnail_file_id'] = $result_upload_thumb[1];
+            $payload['thumbnail_hash_file_receive'] = $result_upload_thumb[0];
+            $payload['snapshot_file_id'] = $result_upload_thumb[1];
+            $payload['snapshot_hash_file_receive'] = $result_upload_thumb[0];
+            $payload['duration'] = $duration;
+        }
+
+        return self::run($payload, "addStory");
+    }
+
+    public function add_video_post($file, $caption, $profile_id, $tumb_image, $duration = 1)
+    {
+
+        if (!is_file($file)) {
+            die("file is not Found");
+        }
+
+        if (is_null($file)) {
+            die("Please Set tumb_image Value");
+        }
+
+        return self::add_post($file, $caption, 'Video', $profile_id, $tumb_image, $duration);
+    }
+
+    public function add_photo_post($file, $caption, $profile_id, $tumb_image = null)
+    {
+
+        if (!is_file($file)) {
+            die("file is not Found");
+        }
+
+        return self::add_post($file, $caption, 'Picture', $profile_id, $tumb_image);
+    }
+
+    public function add_video_story($file, $profile_id, $tumb_image, $duration = 1)
+    {
+
+        if (!is_file($file)) {
+            die("file is not Found");
+        }
+
+        if (is_null($file)) {
+            die("Please Set tumb_image Value");
+        }
+
+        return self::add_story($file, 'Video', $profile_id, $tumb_image, $duration);
+    }
+
+    public function add_photo_story($file, $profile_id, $tumb_image = null)
+    {
+
+        if (!is_file($file)) {
+            die("file is not Found");
+        }
+
+        return self::add_story($file,'Picture', $profile_id, $tumb_image);
+    }
+
+    
 }
-
-?>
-
-
